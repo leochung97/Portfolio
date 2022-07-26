704ba1b1ce13427de5125de7d63071fa0f443257
Leo Chung <leo.chung97@gmail.com>
2022-07-26 17:50:53 -0400

    Anchors tested to work - requires styling + stickiness

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,12 @@ class NavBar extends Component {
   render() {
     return (
       <div>
-        This is your NavBar
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#experience">Experience</a>
+        <a href="#contact">Contact</a>
       </div>
     )
   }
