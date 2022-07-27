5ff59ea522bb31613bb8828096f0b92b08509bd4
Leo Chung <leo.chung97@gmail.com>
2022-07-27 17:06:27 -0400

    Updates

---
+++ precache-manifest.88373f5d3fc40e01b75ec8aaa53e614c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46da4e7f15af147b5da4ba9faf5ebbcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f40edfddac2afcb8c2c",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a1bad8d2b25c324a3e1c",
+    "url": "/static/css/main.d179b70c.chunk.css"
+  },
+  {
+    "revision": "7f40edfddac2afcb8c2c",
+    "url": "/static/js/2.cc4749f1.chunk.js"
+  },
+  {
+    "revision": "2d3407f67e1ce6854fbe05878e8577b3",
+    "url": "/static/js/2.cc4749f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1bad8d2b25c324a3e1c",
+    "url": "/static/js/main.eab0016e.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
